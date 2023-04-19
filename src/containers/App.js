dc022544c8f17b4733c87a73f345c27963e3bfef
kasrus <kruslim@gmail.com>
2023-04-19 14:45:16 -0700

    corrected some errors

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,6 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
-
 import './App.css';
 
 class App extends Component  {
